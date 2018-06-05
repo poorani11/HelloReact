9bb24249e62e17160edbdc3028c94b37edcd9bea
poorani11 <poorani.mgr@gmail.com>
2018-06-05 17:28:57 +0530

    component properties

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,26 @@
+var Greeter = React.createClass({
+    getDefaultProps: function() {
+        return {
+            name: 'React',
+            message:'default message'
+        };
+    },
+    render: function() {
+        var name = this.props.name;
+        var message = this.props.message;
+        return (
+            <div>
+                <h1>Hello {name}!</h1>
+                <p>{message}</p>
+            </div>
+        );
+    }
+});
+
+var firstName = 'Poorani';
+var message = 'This is from the Greeter Component';
+
 ReactDOM.render(
-         <h1>Hello React</h1>,
+         <Greeter name={firstName} message={message}/>,
          document.getElementById('app')
 );
